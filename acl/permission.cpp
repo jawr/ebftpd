bcb33bf1ce0072b0934942b04ff3e54e8b201055
Biohazard <biohazard@hixxy.org>
2012-09-17 14:24:31 -0700

    separated acl.cpp out into acl.cpp and permission.cpp, added acl repository and handler files, updated makefile

---
+++ acl/permission.cpp
@@ -0,0 +1,32 @@
+#include "permission.hpp"
+#include "user.hpp"
+
+namespace acl
+{
+
+Permission* new_clone(Permission const& other)
+{
+  return other.Clone();
+}
+
+boost::tribool FlagPermission::Evaluate(const User& user) const
+{
+  if (flags.find('*') != std::string::npos) return !negate;
+  if (user.CheckFlags(flags)) return !negate;
+  return boost::indeterminate;
+}
+
+boost::tribool UserPermission::Evaluate(const User& user) const
+{
+  if (user.Name() == username) return !negate;
+  return boost::indeterminate;
+}
+
+boost::tribool GroupPermission::Evaluate(const User& user) const
+{
+  (void) user;
+  // unable to convert groupname to gid and vice versa yet!!
+  return boost::indeterminate;
+}
+
+} /* acl namespace */
---
+++ acl/repository.cpp
@@ -0,0 +1,39 @@
+#include <fnmatch.h>
+#include "repository.hpp"
+
+namespace acl
+{
+
+void PathRepository::Insert(const std::string& pathMask, const ACL& acl)
+{
+  acls.push_back(std::make_pair(pathMask, acl));
+}
+
+bool PathRepository::Lookup(const std::string& path, ACL& acl)
+{
+  for (Container::const_iterator it = acls.begin();
+       it != acls.end(); ++it)
+  {
+    if (!fnmatch(it->first.c_str(), path.c_str(), 0))
+    {
+      acl = it->second;
+      return true;
+    }
+  }
+  return false;
+}
+
+void GeneralRepository::Insert(const std::string& key, const ACL& acl)
+{
+  acls.insert(std::make_pair(key, acl));
+}
+
+bool GeneralRepository::Lookup(const std::string& key, ACL& acl)
+{
+  Container::const_iterator it = acls.find(key);
+  if (it == acls.end()) return false;
+  acl = it->second;
+  return true;
+}
+
+} /* acl namespace */
