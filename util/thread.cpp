2149fbdd536ad1ecac68fccd1e5d9c2cf88c7de2
me <me@here.com>
2012-09-18 14:46:23 +0100

    added thread base class

---
+++ util/thread.cpp
@@ -0,0 +1,34 @@
+#include <exception>
+#include <boost/thread/thread.hpp>
+#include "util/thread.hpp"
+namespace util
+{
+Thread::~Thread()
+{
+  thread.interrupt();
+  thread.join();
+}
+
+void Thread::Start()
+{
+  try
+  {
+    Run();
+  }
+  catch (boost::thread_interrupted& interruption)
+  {
+    // interrupted, expected
+  }
+  catch (std::exception& e)
+  {
+    // handle?
+  }
+}
+
+void Thread::Join()
+{
+  thread.join();
+}
+
+// end util namespace
+}
