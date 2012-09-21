c05b34bad066d7e7f5dbfc86e88ea41df590ff56
Biohazard <biohazard@hixxy.org>
2012-09-21 14:16:23 +1000

    lots of changes in both client and listener

--- ftp/client.hpp
+++ ftp/client.hpp
@@ -2,27 +2,45 @@
 #define __FTP_CLIENT_HPP
 
 #include <string>
+#include <boost/thread/mutex.hpp>
 #include "acl/user.hpp"
 #include "util/tcpclient.hpp"
 #include "util/thread.hpp"
+#include "util/error.hpp"
 
 namespace ftp 
 {
 
-class Client : public util::ThreadConsumer
+class Client : public util::ThreadSelect
 {
+  mutable boost::mutex mutex;
   std::string workDir;
   acl::User user;
   util::tcp::client socket;
+  bool finished;
+  int lastCode;
+  char buffer[BUFSIZ];
+  std::string command;
+
+  void SendReply(int code, bool part, const std::string& message);
+  void DisplayWelcome();
+  void NextCommand();
+  void Handle();
   
 public:
-  Client() : workDir("/"), user("biohazard", "somepass", "1") { }
+  Client() : workDir("/"), user("biohazard", "somepass", "1"), lastCode(0)   { }
 
   const std::string& WorkDir() const { return workDir; };
   const acl::User& User() const { return user; }
-
-  util::tcp::client& Socket() { return socket; };
   void Run();
+  
+  void PartReply(int code, const std::string& message);
+  void PartReply(const std::string& message);
+  void Reply(int code, const std::string& message);
+  void Reply(const std::string& message);
+
+  bool Accept(util::tcp::server& server);
+  bool Finished() const;
 };
 
 } /* ftp namespace */
--- ftp/listener.cpp
+++ ftp/listener.cpp
@@ -1,37 +1,109 @@
+#include <memory>
 #include <boost/thread/thread.hpp>
 #include <boost/thread/locks.hpp>
+#include <sys/select.h>
 #include "ftp/listener.hpp"
 #include "ftp/client.hpp"
 #include "logger/logger.hpp"
-#include <iostream>
+#include "util/exception.hpp"
+
 namespace ftp
 {
+
+bool Listener::Initialise()
+{
+  try
+  {
+    server.listen(addr);
+  }
+  catch (const util::network_error& e)
+  {
+    logger::error << "Unable to listen for clients on " << addr << ": " << e.what() << logger::endl;
+    return false;
+  }
+  
+  logger::ftpd << "Listening for clients on " << addr << logger::endl;
+  return true;
+}
+
+void Listener::HandleClients()
+{
+  // erase any clients that are done
+  for (ClientList::iterator it = clients.begin();
+       it != clients.end();)
+  {
+    ftp::Client& client = *it;
+    if (client.Finished())
+    {
+      client.Join();
+      clients.erase(it++);
+    }
+    else ++it;
+  }
+}
+
+void Listener::AcceptClient()
+{
+  std::auto_ptr<ftp::Client> client(new ftp::Client());
+  if (client->Accept(server)) 
+  {
+    client->Start();
+    clients.push_back(client.release());
+  }
+}
+
+void Listener::AcceptClients()
+{
+  fd_set readSet;
+  FD_ZERO(&readSet);
+  FD_SET(server.socket(), &readSet);
+  FD_SET(interruptPipe[0], &readSet);
+  int max = std::max(server.socket(), interruptPipe[0]);
+  
+  struct timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = 100000;
+  
+  int n = select(max + 1, &readSet, NULL, NULL, &tv);
+  if (n < 0)
+  {
+    logger::error << "Listener select failed: " << util::Error::Failure(errno).Message() << logger::endl;
+    // ensure we don't poll rapidly on repeated select failures
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+  }
+  else if (FD_ISSET(server.socket(), &readSet)) AcceptClient();
+  else if (FD_ISSET(interruptPipe[0], &readSet))
+  {
+    boost::this_thread::interruption_point();
+    // possibly use this to refresh our config too ??
+    // if port has changed in config, we have to renew our server object with
+    // new endpoint
+  }
+}
+
 void Listener::Run()
 {
-  logger::ftpd << "Starting listener on " << addr.ip() << ":" << addr.port() << logger::endl;
-  server.listen(addr);
   while (true)
   {
-    // needs to move to some kind of pool. or at least a manager.
-    // colud use a similar pool to what i had before, but this requires a
-    // global object.
-    ftp::Client *client = new ftp::Client();
-    server.accept(client->Socket());
-    client->Start();
-    logger::ftpd << "Got client: " << client->Socket().remote_endpoint().ip() << logger::endl;
-    clients.insert(client);
+    AcceptClients();
+    HandleClients();
   }
 }
 
 }
 // end ftp namespace
 
-#ifdef LISTENER_TEST
+#ifdef FTP_LISTENER_TEST
+
+#include <iostream>
+
 int main()
 {
-  ftp::Listener l;
+  ftp::Listener l("127.0.0.1", 1234);
+  if (!l.Initialise()) return 1;
   l.Start();
   l.Join();
   return 0;
 }
+
 #endif
