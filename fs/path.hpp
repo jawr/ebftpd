0fdff9b2bd70fc7409de1563c95e00b7dc04b376
Biohazard <biohazard@hixxy.org>
2012-09-25 22:31:28 +1000

    added < and > comparison operators to path class and overloaded / + /= += for const char* (already done for std::string), fixed bug causing double slash when joining paths

--- fs/path.hpp
+++ fs/path.hpp
@@ -28,6 +28,13 @@ public:
   
   Path& operator/=(const std::string& rhs) { return Join(rhs); }
   Path& operator+=(const std::string& rhs) { return Append(rhs); }
+
+  Path& operator/=(const char* rhs) { return Join(rhs); }
+  Path& operator+=(const char* rhs) { return Append(rhs); }
+
+  bool operator<(const Path& rhs) const { return path < rhs.path; }
+  bool operator>(const Path& rhs) const { return path > rhs.path; }
+  
   operator std::string() { return path; }
   operator std::string() const { return path; }
   
@@ -35,10 +42,13 @@ public:
   bool Empty() const { return path.empty(); }
   
   const char* CString() const { return path.c_str(); }
+  const std::string& ToString() const { return path; }
 };
 
 Path operator/(const Path& lhs, const std::string& rhs);
 Path operator+(const Path& lhs, const std::string& rhs);
+Path operator/(const Path& lhs, const char* rhs);
+Path operator+(const Path& lhs, const char* rhs);
 std::ostream& operator<<(std::ostream& os, const Path& path);
 std::istream& operator>>(std::istream& is, Path& path);
 
