e1ab6e3d0635c8bba7bd753b5f8597ac0c80c5a8
me <me@here.com>
2012-10-04 00:16:16 +0100

    fixed compiling issues with util/string

--- util/string.hpp
+++ util/string.hpp
@@ -2,18 +2,9 @@
 #define __UTIL_STRING_HPP
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string.hpp>
-namespace util
+namespace util { namespace string
 {
-namespace string
-{
-  bool BoolLexicalCast(const std::string& arg)
-  {
-    std::string arg_ = arg;
-    boost::algorithm::to_lower(arg_);
-    if (arg_ == "yes") return true;
-    else if (arg_ == "no") return false;
-    return boost::lexical_cast<bool>(arg);
-  }
+  bool BoolLexicalCast(const std::string& arg);
 }
 }
 #endif
