9161fc20dac8cf2fa053044c463c3aaf0d9c1218
Biohazard <biohazard@hixxy.org>
2012-10-12 09:28:03 +1000

    added cmd/site folder

---
+++ cmd/site/factory.cpp
@@ -0,0 +1,62 @@
+#include <boost/algorithm/string.hpp>
+#include "cmd/site/factory.hpp"
+
+namespace cmd { namespace site
+{
+
+Factory Factory::factory;
+
+Factory::Factory()
+{
+}
+
+Factory::~Factory()
+{
+  while (!creators.empty())
+  {
+    delete creators.begin()->second;
+    creators.erase(creators.begin());
+  }
+}
+
+void Factory::Register(const std::string& command,
+                       CreatorBase<cmd::Command>* creator)
+{
+  creators.insert(std::make_pair(command, creator));
+}  
+
+cmd::Command* Factory::Create(ftp::Client& client, const std::string& argStr,
+                              const Args& args)
+{
+  std::string cmd = args[0];
+  boost::to_upper(cmd);
+  CreatorsMap::const_iterator it = factory.creators.find(cmd);
+  if (it == factory.creators.end()) return nullptr;
+  return it->second->Create(client, argStr, args);
+}
+
+} /* site namespace */
+} /* cmd namespace */
+
+#ifdef CMD_SITE_FACTORY_TEST
+
+#include <iostream>
+#include <memory>
+#include "ftp/client.hpp"
+
+int main()
+{
+  using namespace cmd;
+  
+  ftp::ClientState::Client client;
+  Args args;
+  args.push_back("USER");
+  std::unique_ptr<cmd::Command> cmd(Factory::Create(client, args));
+  cmd->Execute();
+  args.clear();
+  args.push_back("PASS");
+  cmd.reset(Factory::Create(client, args));
+  cmd->Execute();
+}
+
+#endif
