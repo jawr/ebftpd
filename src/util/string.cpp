45dd09666db3ca8c63a0f0a2b77addd16ffdea3c
biohazard <biohazard@hixxy.org>
2012-10-16 20:09:39 +1000

    some new string tools

--- src/util/string.cpp
+++ src/util/string.cpp
@@ -1,3 +1,4 @@
+#include <cctype>
 #include <fnmatch.h>
 #include "util/string.hpp"
 
@@ -18,6 +19,69 @@ bool WildcardMatch(const std::string& pattern,
   return !fnmatch(pattern.c_str(), str.c_str(), iCase ? FNM_CASEFOLD : 0);
 }
 
+std::string::size_type 
+FindNthChar(const std::string& str, char ch, unsigned n)
+{
+  std::string::size_type pos = str.find_first_of(ch);
+  while (--n && pos != std::string::npos)
+    pos = str.find_first_of(ch, pos + 1);
+  return pos;
+}
+
+std::string::size_type
+FindNthNonConsecutiveChar(const std::string& str, 
+    char ch, unsigned n)
+{
+  std::string::size_type pos = str.find_first_of(ch);
+  while (--n && pos != std::string::npos)
+  {
+    while (str[pos] == ch && pos != std::string::npos)
+      ++pos;
+    if (pos == std::string::npos) break;
+    pos = str.find_first_of(ch, pos + 1);
+  }
+  return pos;
+}
+
+std::string& CompressWhitespace(std::string& str)
+{
+  for (auto it = str.begin(); it != str.end();)
+    if (std::isspace(*it++))
+      while (it != str.end() && std::isspace(*it))
+        it = str.erase(it);
+
+  if (str.empty()) return str;
+  if (std::isspace(*str.begin())) 
+    str.erase(str.begin());
+  
+  if (str.empty()) return str;
+  if (std::isspace(*(str.end() - 1)))
+    str.erase(str.end() - 1);
+    
+  return str;
+}
+
+std::string CompressWhitespaceCopy(const std::string& str)
+{
+  std::string temp(str);
+  return CompressWhitespace(temp);
+}
+
 } /* string namespace */
 } /* util namespace */
 
+#ifdef UTIL_STRING_TEST
+
+#include <iostream>
+
+int main()
+{
+  using namespace util::string;
+
+  std::string s = "    hello how   are you   today !";
+  
+  std::cout << CompressWhitespace(s) << std::endl;
+  
+}
+
+#endif
--- src/util/string.hpp
+++ src/util/string.hpp
@@ -12,6 +12,16 @@ bool BoolLexicalCast(std::string arg);
 bool WildcardMatch(const std::string& pattern,
                    const std::string& str, bool iCase = false);
 
+std::string::size_type 
+FindNthChar(const std::string& str, char ch, unsigned n);
+
+std::string::size_type
+FindNthNonConsecutiveChar(const std::string& str, 
+    char ch, unsigned n);
+
+std::string& CompressWhitespace(std::string& str);
+std::string CompressWhitespaceCopy(const std::string& str);
+    
 } /* string namespace */
 } /* util namespace */
 
