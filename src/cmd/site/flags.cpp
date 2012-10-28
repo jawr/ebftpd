4c4a4e66480b0774fe23ed8dda00a59bcf36ca33
biohazard <biohazard@hixxy.org>
2012-10-28 18:07:07 +1000

    implemented flagsown acl keyword and tidied up flags command formatting

--- src/cmd/site/flags.cpp
+++ src/cmd/site/flags.cpp
@@ -4,12 +4,18 @@
 #include "acl/usercache.hpp"
 #include "acl/flags.hpp"
 #include "util/error.hpp"
+#include "acl/allowsitecmd.hpp"
 
 namespace cmd { namespace site
 {
 
 cmd::Result FLAGSCommand::Execute()
 {
+  if (args.size() == 2 && !acl::AllowSiteCmd(client.User(), "flags"))
+  {
+    return cmd::Result::Permission;
+  }
+
   acl::User user(client.User());
   if (args.size() == 2)
   {
@@ -26,28 +32,28 @@ cmd::Result FLAGSCommand::Execute()
   
   std::ostringstream os;
   os << "FLAGS for " << user.Name() << "\n\n";
-  os << CheckFlag(user, acl::Flag::FlagSiteop) << " Siteop -1-\n";
-  os << CheckFlag(user, acl::Flag::Gadmin) << " Gadmin -2-\n";
-  os << CheckFlag(user, acl::Flag::Glock) << " Glock -3-\n";
-  os << CheckFlag(user, acl::Flag::Exempt) << " Exempt -4-\n";
-  os << CheckFlag(user, acl::Flag::Color) << " Color -5-\n";
-  os << CheckFlag(user, acl::Flag::Deleted) << " Deleted -6-\n";
-  os << CheckFlag(user, acl::Flag::Useredit) << " User Edit -7-\n";
-  os << CheckFlag(user, acl::Flag::Anonymous) << " Anonymous -8-\n";
-  os << CheckFlag(user, acl::Flag::Nuke) << " Nuke -A-\n";
-  os << CheckFlag(user, acl::Flag::Unnuke) << " Unnuke -B-\n";
-  os << CheckFlag(user, acl::Flag::Undupe) << " Undupe -C-\n";
-  os << CheckFlag(user, acl::Flag::Kick) << " Kick -D-\n";
-  os << CheckFlag(user, acl::Flag::Kill) << " Kill -E-\n";
-  os << CheckFlag(user, acl::Flag::Take) << " Take -F-\n";
-  os << CheckFlag(user, acl::Flag::Give) << " Give -G-\n";
-  os << CheckFlag(user, acl::Flag::Users) << " Users -H-\n";
-  os << CheckFlag(user, acl::Flag::Idler) << " Idler -I-\n";
-  os << CheckFlag(user, acl::Flag::Custom1) << " Custom 1 -J-\n";
-  os << CheckFlag(user, acl::Flag::Custom2) << " Custom 2 -K-\n";
-  os << CheckFlag(user, acl::Flag::Custom3) << " Custom 3 -L-\n";
-  os << CheckFlag(user, acl::Flag::Custom4) << " Custom 4 -M-\n";
-  os << CheckFlag(user, acl::Flag::Custom5) << " Custom 5 -N-\n\n";
+  os << CheckFlag(user, acl::Flag::FlagSiteop)  << " Siteop    -1-\n";
+  os << CheckFlag(user, acl::Flag::Gadmin)      << " Gadmin    -2-\n";
+  os << CheckFlag(user, acl::Flag::Glock)       << " Glock     -3-\n";
+  os << CheckFlag(user, acl::Flag::Exempt)      << " Exempt    -4-\n";
+  os << CheckFlag(user, acl::Flag::Color)       << " Color     -5-\n";
+  os << CheckFlag(user, acl::Flag::Deleted)     << " Deleted   -6-\n";
+  os << CheckFlag(user, acl::Flag::Useredit)    << " User Edit -7-\n";
+  os << CheckFlag(user, acl::Flag::Anonymous)   << " Anonymous -8-\n";
+  os << CheckFlag(user, acl::Flag::Nuke)        << " Nuke      -A-\n";
+  os << CheckFlag(user, acl::Flag::Unnuke)      << " Unnuke    -B-\n";
+  os << CheckFlag(user, acl::Flag::Undupe)      << " Undupe    -C-\n";
+  os << CheckFlag(user, acl::Flag::Kick)        << " Kick      -D-\n";
+  os << CheckFlag(user, acl::Flag::Kill)        << " Kill      -E-\n";
+  os << CheckFlag(user, acl::Flag::Take)        << " Take      -F-\n";
+  os << CheckFlag(user, acl::Flag::Give)        << " Give      -G-\n";
+  os << CheckFlag(user, acl::Flag::Users)       << " Users     -H-\n";
+  os << CheckFlag(user, acl::Flag::Idler)       << " Idler     -I-\n";
+  os << CheckFlag(user, acl::Flag::Custom1)     << " Custom 1  -J-\n";
+  os << CheckFlag(user, acl::Flag::Custom2)     << " Custom 2  -K-\n";
+  os << CheckFlag(user, acl::Flag::Custom3)     << " Custom 3  -L-\n";
+  os << CheckFlag(user, acl::Flag::Custom4)     << " Custom 4  -M-\n";
+  os << CheckFlag(user, acl::Flag::Custom5)     << " Custom 5  -N-\n\n";
 
   os << "A '*' denotes an enabled flag.  Only sysop users may change flags.\n";
   os << "Use SITE CHANGE <USER> FLAGS +/-VALUE to change.";
