b6965b4e78b5b70f191dfdf79bf6263a8d2a3267
me <jawr@jarrah>
2012-11-06 18:12:55 +0000

    Fixed include issues caused by unity. Added the conversion files missed on last commit.

---
+++ src/stats/conversions.hpp
@@ -0,0 +1,22 @@
+#ifndef __STATS_CONVERSIONS_HPP
+#define __STATS_CONVERSIONS_HPP
+
+#include <string>
+#include "stats/stat.hpp"
+
+namespace stats
+{
+
+namespace tostring
+{
+
+std::string Byte(const Stat& stat);
+std::string Kbyte(const Stat& stat);
+std::string Mbyte(const Stat& stat);
+std::string Gbyte(const Stat& stat);
+
+}
+
+// end
+}
+#endif
