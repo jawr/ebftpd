6476be6c0d2fddf7e5e89e6fbb342594f45d39fc
biohazard <biohazard@hixxy.org>
2012-11-07 19:45:45 +1000

    removed not needed SpeedControl() definition

--- src/cmd/rfc/retr.hpp
+++ src/cmd/rfc/retr.hpp
@@ -8,8 +8,6 @@ namespace cmd { namespace rfc
 
 class RETRCommand : public Command
 {
-  void SpeedControl();
-  
 public:
   RETRCommand(ftp::Client& client, const std::string& argStr, const Args& args) :
     Command(client, client.Control(), client.Data(), argStr, args) { }
