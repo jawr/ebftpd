3291cc4c5e9220f790cb7801fc1bf457d9a9b79b
biohazard <biohazard@hixxy.org>
2012-11-15 14:43:31 +1000

    made util::Pipe noncopyable and added Reset and Close member functions

--- src/util/pipe.hpp
+++ src/util/pipe.hpp
@@ -3,12 +3,13 @@
 
 #include <unistd.h>
 #include <cerrno>
+#include <boost/noncopyable.hpp>
 #include "util/error.hpp"
 
 namespace util
 {
 
-class Pipe
+class Pipe : boost::noncopyable
 {
   int fds[2];
 
@@ -18,14 +19,43 @@ public:
     if (pipe(fds) < 0) throw SystemError(errno);
   }
   
-  ~Pipe()
-  {
-    close(fds[0]);
-    close(fds[1]);
-  }
-
   int ReadFd() const { return fds[0]; }
   int WriteFd() const { return fds[1]; }  
+  
+  void CloseRead()
+  {
+    if (fds[0] >= 0)
+    {
+      close(fds[0]); 
+      fds[0] = -1;
+    }
+  }
+  
+  void CloseWrite()
+  {
+    if (fds[1] >= 0)
+    {
+      close(fds[1]);
+      fds[1] = -1;
+    }
+  }
+  
+  void Close()
+  {
+    CloseRead();
+    CloseWrite();
+  }
+  
+  ~Pipe()
+  {
+    Close();
+  }
+  
+  void Reset()
+  {
+    Close();
+    if (pipe(fds) < 0) throw SystemError(errno);
+  }
 };
 
 } /* util namespace */
